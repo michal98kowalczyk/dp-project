92c24d04fdb654e6a668c5ed4ae88e5c45c224ca
Michal Kowalczyk <michal98kowalczyk@gmail.com>
2021-01-06 13:57:41 +0100

    added basic properties

---
+++ src/main/java/dp/orm/OrmApp.java
@@ -0,0 +1,13 @@
+package dp.orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmApp {
+    public static void main(String[] args){
+
+        SpringApplication.run(OrmApp.class,args);
+
+    }
+}
