c7ae72f3ce7cb573385caa6adeea91dc7776bfd0
Michal Kowalczyk <michal98kowalczyk@gmail.com>
2021-01-08 16:07:47 +0100

    added findUtils class which can find all fields in classes hierarchy

---
+++ src/main/java/dp/orm/utlis/FieldUtils.java
@@ -0,0 +1,38 @@
+package dp.orm.utlis;
+
+import dp.orm.annotations.DatabaseField;
+import lombok.Data;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FieldUtils {
+
+    public static <T> List<Field> getAllFields(Class<T> cls){
+
+        List<Field> fields = new ArrayList<Field>();
+
+        Field[] declaredFieldsInClass = cls.getDeclaredFields();
+        fields.addAll(Arrays.asList(declaredFieldsInClass));
+
+        Class<? super T> superClass = cls.getSuperclass();
+
+        while (superClass != null && !superClass.equals(Object.class)){
+            fields.addAll(Arrays.asList(superClass.getDeclaredFields()));
+
+            superClass = superClass.getSuperclass();
+        }
+
+
+//        return only fields which are fields of database, so check if our annotation is in class
+        return fields.stream().filter(field -> field.isAnnotationPresent(DatabaseField.class)
+        && !field.getName().startsWith("this")).collect(Collectors.toList());
+    }
+
+
+
+
+}
