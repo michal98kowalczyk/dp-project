d229d0b1fa2da5fffa7e1af61655391675331d3a
Michal Kowalczyk <michal98kowalczyk@gmail.com>
2021-01-22 14:56:21 +0100

    add table schema and fix connection with inheritance mapping

---
+++ src/main/java/dp/orm/schemas/TableSchema.java
@@ -0,0 +1,37 @@
+package dp.orm.schemas;
+
+
+import lombok.*;
+
+import java.util.Collection;
+import java.util.Set;
+
+
+@ToString
+@RequiredArgsConstructor
+@EqualsAndHashCode
+@Setter
+@Getter
+@Builder
+public class TableSchema {
+
+    private Class cls;
+
+    private String name;
+
+    private ColumnSchema id;
+
+    private Set<ColumnSchema> columns;
+
+    public void addColumn(ColumnSchema column){
+        columns.add(column);
+    }
+
+    public void addColumns(Collection<ColumnSchema> columns){
+        columns.addAll(columns);
+    }
+
+
+
+
+}
